9a957116ca64a45fbdd1c8c8c5258d18ca1d81e8
sumit <singhsk242004@gmail.com>
2025-05-17 02:09:24 +0530

    firstt commit

---
+++ scrapper.go
@@ -0,0 +1,11 @@
+import (
+	"fmt"
+	"context"
+	"strings"
+	"log"
+
+	"github.com/gocolly/colly/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
